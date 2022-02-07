6c3d492458eae4600fde9995862d285d299b2bbc
robsca <66328669+robsca@users.noreply.github.com>
2022-02-07 15:47:49 +0000

    Update run.py

--- run.py
+++ run.py
@@ -16,18 +16,18 @@ location = geolocator.geocode(str(address_))
 
 # Show results 
 if st.button('Search'):
-try:
-    # Create map
-    address = location.address
-    column1, column2 = st.columns(2)
-    with column1:
-        # plot map crime density
-        from get_map_plot import get_data_and_plot
-        a = get_data_and_plot(location.latitude, location.longitude)
-    with column2:
-        # get categories - pie chart
-        from get_categories_piechart import categories
-        cat = categories(location.latitude, location.longitude)
-except:
-    st.subheader("Select a valid location)
-    st.subheader("Make sure you put the space in the right spot - ex. N1 1BB")
+    try:
+        # Create map
+        address = location.address
+        column1, column2 = st.columns(2)
+        with column1:
+            # plot map crime density
+            from get_map_plot import get_data_and_plot
+            a = get_data_and_plot(location.latitude, location.longitude)
+        with column2:
+            # get categories - pie chart
+            from get_categories_piechart import categories
+            cat = categories(location.latitude, location.longitude)
+    except:
+        st.subheader("Select a valid location)
+        st.subheader("Make sure you put the space in the right spot - ex. N1 1BB")
